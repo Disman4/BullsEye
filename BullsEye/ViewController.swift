14f952e6187e859670a41d0391b9887ee80ddcc8
tanaka <tanaka.mawoyo@yahoo.com>
2023-04-11 17:31:48 +0200

    target label updates

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,17 +10,33 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 20
+    var targetValue = 0
     
     @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    //function to update the labels (target, score, round)
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
     }
     
     @IBAction func showAlert(){
-        let messege = "The value of the slider is : \(currentValue)"
+        let messege = "The value of the slider is : \(currentValue)" + "\n The target is: \(targetValue)"
+        
         
         let alert = UIAlertController(title: "Hello!", message: messege, preferredStyle: .alert)
         
@@ -28,6 +44,8 @@ class ViewController: UIViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
 
     @IBAction func SliderMoved(_ slider: UISlider){
